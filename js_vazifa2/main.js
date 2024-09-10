aba47a7478c239cd2bd478e6a072273db1c4ff3d
latif <latifjonsheraliyev209@gmail.com>
2024-09-10 07:57:12 +0500

    js vazifa 2

---
+++ js_vazifa2/main.js
@@ -0,0 +1,89 @@
+// 1-masala
+// let son=10;
+// for(let i=1;i<=son;i++){
+//     console.log(i);
+// }
+
+// 2-masala
+// let son=1;
+// for(let i=10;i>=son;i--){
+//     console.log(i);
+// }
+
+// 3-masala
+// let juft=20;
+// for(let i=1;i<=juft;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// 4-masala
+// let toq=20;
+// for(let i=1;i<=toq;i++){
+//     if(i%2){
+//         console.log(i)
+//     }
+// }
+
+// 5-masala
+// let a=+prompt("son kirting: ");
+// let k=1;
+// for (let i=1;i<=a;i++){
+//    k*=i;
+// }
+// console.log(k);
+
+
+// 9-masala
+// let str = prompt("matn yoki raqam kiriting: "); 
+// for (let i = 0; i < str.length; i++) {
+//   if (!isNaN(str[i]) && str[i] !== ' ') {
+//     console.log(str[i]); 
+//   }
+// }
+
+// 10-masala
+// let str=prompt("matn kirting: ")
+// let count=0;
+// for (let i=0;i<=str.length;i++){
+//   if (str[i]===" "){
+//      count++;
+//   }
+// }
+// console.log(count)
+
+
+// 11-masala
+
+
+// unli harf
+// let s = prompt("Matn kiriting: ");
+// for (let i = 0; i < s.length; i++) {
+//     if (s[i] === "a") {
+//         console.log(`a harfi indeksda: ${i}`);
+//     } else if (s[i] === "e") { 
+//         console.log(`e harfi indeksda: ${i}`);
+//     } else if (s[i] === "i") {
+//         console.log(`i harfi indeksda: ${i}`);
+//     } else if (s[i] === "o") {
+//         console.log(`o harfi indeksda: ${i}`);
+//     } else if (s[i] === "u") {
+//         console.log(`u harfi indeksda: ${i}`);
+//     } else {
+//         console.log(`Indeks ${i}: unli harf yo'q`);
+//     }
+// }
+
+// 16_masala
+// let srt = prompt("Matn kiriting: ");
+// let srt2 = prompt("Oxirgi harfni qaysi harfga almashtirishni hohlaysiz?");
+
+// let newStr = srt.slice(0, srt.length - 1) + srt2;
+
+// alert("Yangi matn: " + newStr);
+
+
+
+
+
